dba062bebad9760206a33216c91ce81aa02b6b4d
mehdi <elmehdi.alhebaby@edu.uca.ac.ma>
2021-08-14 21:54:24 +0000

    modified the list and the dictionary info

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ class User(Screen):
     phy=0
     bio=0
     autr=0
+    data_info = [{"isbn": "1", "name": "info1", "categorie": "info","Disponible":"oui"},
+                 {"isbn": "2", "name": "info2", "categorie": "info","Disponible":"oui"},
+                 {"isbn": "3", "name": "info3", "categorie": "info","Disponible":"oui"}]
+    name_info = ["info","Info","informatique","Informatique"]
     def table(self):
         aff_tab=self.ids.afftable
         details = BoxLayout(size_hint_y=None, height=30, pos_hint={"top": 1})
@@ -32,25 +36,24 @@ class User(Screen):
             self.info=1
            # self.table()
             aff_lol=self.ids.aff
-            #for i in len(data_info):
-            #    isbn=data[i][isbn]
-            details = BoxLayout(size_hint_y=None, height=30, pos_hint={"top": 1})
-            aff_lol.add_widget(details)
-            isbn = Label(text='123', size_hint_x=.15, color=(0, 0, 0, 1))
-            titre = Label(text='info', size_hint_x=.4, color=(0, 0, 0, 1))
-            Categorie = Label(text='info', size_hint_x=.2, color=(0, 0, 0, 1))
-            Disponible = Label(text='oui', size_hint_x=.25, color=(0, 0, 0, 1))
-            details.add_widget(isbn)
-            details.add_widget(titre)
-            details.add_widget(Categorie)
-            details.add_widget(Disponible)
+            for i in range(len(self.data_info)):
+                details = BoxLayout(size_hint_y=None, height=30, pos_hint={"top": 1})
+                aff_lol.add_widget(details)
+                isbn = Label(text=self.data_info[i]["isbn"], size_hint_x=.15, color=(0, 0, 0, 1))
+                titre = Label(text=self.data_info[i]["name"], size_hint_x=.4, color=(0, 0, 0, 1))
+                Categorie = Label(text=self.data_info[i]["categorie"], size_hint_x=.2, color=(0, 0, 0, 1))
+                Disponible = Label(text=self.data_info[i]["Disponible"], size_hint_x=.25, color=(0, 0, 0, 1))
+                details.add_widget(isbn)
+                details.add_widget(titre)
+                details.add_widget(Categorie)
+                details.add_widget(Disponible)
     def info_button(self, widjet):
         self.ids.chercher.text = "chercher"
         if widjet.state == "down" and self.info == 0:
             self.informatique(widjet)
     def chercher(self,widjet):
         book=self.ids.book_name.text
-        if  book=="info"or book=="Info"or book=="informatique"or book=="Informatique":
+        if  book in self.name_info:
             if self.info==0:
                 self.info=1
                 self.informatique(widjet)
@@ -62,9 +65,9 @@ kv = Builder.load_file('My.kv')
 class Myapp(App):
     def build(self):
         return kv
-data_info=[{"isbn":"1","name":"info1","categorie":"info"},
-      {"isbn":"1","name":"info2","categorie":"info"},
-      {"isbn":"3","name":"info3","categorie":"info"}]
-book_recherch_info=["info","Info","informatique","Informatique"]
 if __name__=='__main__':
     Myapp().run()
+
+
+
+#book_recherch_info=["info","Info","informatique","Informatique"]

7c5f82f1b9e422a84c6fc7f1741b7055898842ba
mehdi <elmehdi.alhebaby@edu.uca.ac.ma>
2021-08-23 14:33:31 +0000

    create fonction search

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.label import Label
 import mysql.connector
-from kivymd.app import MDApp
 
 mydb=mysql.connector.connect(
     host="b8fe7neoqvypvqfx1i0y-mysql.services.clever-cloud.com",
@@ -69,6 +68,10 @@ print(livre_info)
 print(livre_math)
 print(livre_aut)
 print(livre_bio)
+liste_recherch_info=['Informatique','informatique','info']
+liste_recherch_math=['Maths','Math','maths','math','Mathématique','mathématique']
+liste_recherch_pc=['pc','Pc','physique','Phyqique','Physique, Chimie']
+liste_recherch_bio=['Biologie, Géologie','Bio','bio','biologie','Biologie','géologie','Géologie','geologie','Geologie']
 class Accueil(Screen):
     def signin(self):
         a = 0
@@ -93,10 +96,6 @@ class User(Screen):
     phy=0
     bio=0
     autr=0
-    data_info = [{"isbn": "1", "name": "info1", "categorie": "info","Disponible":"oui"},
-                 {"isbn": "2", "name": "info2", "categorie": "info","Disponible":"oui"},
-                 {"isbn": "3", "name": "info3", "categorie": "info","Disponible":"oui"}]
-    name_info = ["info","Info","informatique","Informatique"]
     def __init__(self,table='', **kwargs):
         super().__init__(**kwargs)
 
@@ -112,83 +111,133 @@ class User(Screen):
         details.add_widget(Titre)
         details.add_widget(Categorie)
         details.add_widget(Disponible)
-    def informatique(self,widjet):
-            self.info=1
-           # self.table()
-            aff_lol=self.ids.aff
-            for i in range(len(self.data_info)):
-                details = BoxLayout(size_hint_y=None, height=30, pos_hint={"top": 1})
-                aff_lol.add_widget(details)
-                isbn = Label(text=self.data_info[i]["isbn"], size_hint_x=.15, color=(0, 0, 0, 1))
-                titre = Label(text=self.data_info[i]["name"], size_hint_x=.4, color=(0, 0, 0, 1))
-                Categorie = Label(text=self.data_info[i]["categorie"], size_hint_x=.2, color=(0, 0, 0, 1))
-                Disponible = Label(text=self.data_info[i]["Disponible"], size_hint_x=.25, color=(0, 0, 0, 1))
-                details.add_widget(isbn)
-                details.add_widget(titre)
-                details.add_widget(Categorie)
-                details.add_widget(Disponible)
-    def change_screen(self,instance):
-        if instance.text=='Informatique':
-            self.ids.scr_mng.current = 'scr_info'
-        elif instance.text=='Mathématique':
-            self.ids.scr_mng.current = 'scr_math'
-        elif instance.text=="Physique, Chimie":
-            self.ids.scr_mng.current = 'scr_phy'
-        elif instance.text=="Biologie, Géologie":
-            self.ids.scr_mng.current = 'scr_bio'
-        elif instance.text=="Autre":
-            self.ids.scr_mng.current = 'scr_autre'
-        print(self.ids.scr_mng.current)
-    def chercher(self,widjet):
-        book=self.ids.book_name.text
-        if  book in self.name_info:
-            if self.info==0:
-                self.info=1
-                self.informatique(widjet)
 
-    def test(self,instance):
-        if instance.state == "down":
-            if instance.text == 'Informatique' :
-                self.ids.scr_mng.current = 'scr_info'
-                livre_diff = livre_info
-                table_floor_layout=self.ids.table_floor_layout_info
-                table_floor=self.ids.table_floor_info
-            elif instance.text == 'Mathématique':
-                self.ids.scr_mng.current = 'scr_math'
-                livre_diff = livre_math
-                table_floor_layout = self.ids.table_floor_layout_math
-                table_floor = self.ids.table_floor_math
-            elif instance.text == "Physique, Chimie":
-                self.ids.scr_mng.current = 'scr_phy'
-                livre_diff=livre_pc
-                table_floor_layout=self.ids.table_floor_layout_pc
-                table_floor = self.ids.table_floor_pc
-            elif instance.text == "Biologie, Géologie":
-                self.ids.scr_mng.current = 'scr_bio'
-                livre_diff = livre_bio
-                table_floor_layout = self.ids.table_floor_layout_bio
-                table_floor = self.ids.table_floor_bio
-            elif instance.text == "Autre":
-                self.ids.scr_mng.current = 'scr_autre'
-                livre_diff = livre_aut
-                table_floor_layout = self.ids.table_floor_layout_autre
-                table_floor = self.ids.table_floor_autre
+    def table(self,listt,table_floor_layout,table_floor):
+        self.columns = 4
+        table_data = []
+        titrr = ['ISBN', 'Titre', 'Auteur', 'Desponile']
+        for y in range(4):
+            table_data.append({'text': titrr[y], 'size_hint_y': None, 'height': 30,
+                               'bcolor': (.05, .30, .80, 1)})  # append the data
+            y = y + 1
+
+        for z in range(len(listt)):
+            for y in range(4):
+                table_data.append({'text': str(listt[z][y + 1]), 'size_hint_y': None, 'height': 20,
+                                   'bcolor': (.06, .25, .50, 1)})  # append the data
+
+        table_floor_layout.cols = self.columns  # define value of cols to the value of self.columns
+        table_floor.data = table_data  # add table_data to data value
+
+    def test_titre(self,titre):
+        a=0
+        if titre in liste_recherch_info :
+            self.ids.scr_mng.current = 'scr_info'
+            livre_diff = livre_info
+            table_floor_layout=self.ids.table_floor_layout_info
+            table_floor=self.ids.table_floor_info
+            a=1
+        elif titre in liste_recherch_math:
+            self.ids.scr_mng.current = 'scr_math'
+            livre_diff = livre_math
+            table_floor_layout = self.ids.table_floor_layout_math
+            table_floor = self.ids.table_floor_math
+            a=2
+        elif titre in liste_recherch_pc:
+            self.ids.scr_mng.current = 'scr_phy'
+            livre_diff=livre_pc
+            table_floor_layout=self.ids.table_floor_layout_pc
+            table_floor = self.ids.table_floor_pc
+            a=3
+        elif titre in liste_recherch_bio:
+            self.ids.scr_mng.current = 'scr_bio'
+            livre_diff = livre_bio
+            table_floor_layout = self.ids.table_floor_layout_bio
+            table_floor = self.ids.table_floor_bio
+            a=4
+        elif titre == "Autre":
+            self.ids.scr_mng.current = 'scr_autre'
+            livre_diff = livre_aut
+            table_floor_layout = self.ids.table_floor_layout_autre
+            table_floor = self.ids.table_floor_autre
+            a=5
         else:
             self.ids.scr_mng.current = 'scr'
         if self.ids.scr_mng.current != 'scr':
-            self.columns = 5
-            table_data = []
-            for y in range(5) :
-                table_data.append({'text':str(y), 'size_hint_y': None, 'height': 30,
-                                   'bcolor': (.05, .30, .80, 1)})  # append the data
+            self.table(livre_diff,table_floor_layout,table_floor)
+        return a
 
-            for z in range(len(livre_diff)):
-                for y in range(5):
-                    table_data.append({'text': str(livre_diff[z][y]), 'size_hint_y': None, 'height': 20,
-                                       'bcolor': (.06, .25, .50, 1)})  # append the data
+    def recherche(self):
+        book=self.ids.book_name.text
+        a=self.test_titre(book)
+        if a==0 and book != '':
+            b = 0
+            self.ids.mathematique.state="normal"
+            self.ids.info.state="normal"
+            self.ids.pc.state="normal"
+            self.ids.bio.state="normal"
+            self.ids.autre.state="normal"
+            table_floor_layout= self.ids.table_floor_layout_rech
+            table_floor= self.ids.table_floor_rech
+            livre_choix = []
+            for x in livres:
+                if book==str(x[2]):
+                    livre_choix.append(x)
+                    b=1
+            if b==0 :
+                for x in livres:
+                    if book==str(x[1]):
+                        livre_choix.append(x)
+                        b=1
+            if b==0 :
+                for x in livres:
+                    if book==str(x[3]):
+                        livre_choix.append(x)
+                        b=1
+            if b==1:
+                self.ids.scr_mng.current = 'scr_rech'
+                self.table(livre_choix, table_floor_layout, table_floor)
+        elif a==1:
+            self.ids.mathematique.state="normal"
+            self.ids.pc.state="normal"
+            self.ids.bio.state="normal"
+            self.ids.autre.state="normal"
+            self.test_titre(book)
+        elif a==2:
+            self.ids.mathematique.state="down"
+            self.ids.info.state="normal"
+            self.ids.pc.state="normal"
+            self.ids.bio.state="normal"
+            self.ids.autre.state="normal"
+            self.test_titre(book)
+        elif a==3:
+            self.ids.mathematique.state="normal"
+            self.ids.info.state="normal"
+            self.ids.bio.state="normal"
+            self.ids.pc.state="down"
+            self.ids.autre.state="normal"
+            self.test_titre(book)
+        elif a==4:
+            self.ids.mathematique.state="normal"
+            self.ids.info.state="normal"
+            self.ids.pc.state="normal"
+            self.ids.bio.state="down"
+            self.ids.autre.state="normal"
+            self.test_titre(book)
+        elif a==5:
+            self.ids.mathematique.state="normal"
+            self.ids.info.state="normal"
+            self.ids.pc.state="normal"
+            self.ids.bio.state="normal"
+            self.ids.autre.state="down"
+            self.test_titre(book)
 
-            table_floor_layout.cols = self.columns  # define value of cols to the value of self.columns
-            table_floor.data = table_data  # add table_data to data value
+    def test(self,instance):
+        if instance.state == "down":
+            self.test_titre(instance.text)
+        else:
+            self.ids.scr_mng.current = 'scr'
+
 class Profil(Screen):
     pass
 class Paswdmodif(Screen):
